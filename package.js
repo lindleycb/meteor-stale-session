70933181d14bdf21d1fb2ef0d1986f7ed0d4da23
Chris Lindley <chris.lindley@zuuk.com>
2014-08-27 11:15:55 +0100

    Upgrading to meteor 0.9.0.

--- package.js
+++ package.js
@@ -2,13 +2,13 @@ Package.describe({
   name:    'zuuk:stale-session',
   summary: 'Stale session and session timeout handling for meteorjs',
   git:     "https://github.com/lindleycb/meteor-stale-session.git",
-  version: "1.0.5"
+  version: "1.0.6"
 });
 
 Package.onUse(function(api) {
     api.versionsFrom('METEOR-CORE@0.9.0');
-    api.use('accounts-base', ['client','server']);
-    api.use('jquery', 'client');
+    api.use('accounts-base@1.0.0', ['client','server']);
+    api.use('jquery@1.0.0', 'client');
     api.addFiles('client.js', 'client');
     api.addFiles('server.js', 'server');
 });

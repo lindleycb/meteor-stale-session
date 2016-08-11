f6410a321c3127f312398d5be2d3f5c169fd5c86
Pete Corey <petecorey@gmail.com>
2016-08-11 12:17:38 -0400

    Force Logout Config Option
    Gives config option to disable logout and clearing of heartbeat of stale
    user sessions.

--- server.js
+++ server.js
@@ -4,9 +4,11 @@
 // Meteor settings:
 // - staleSessionInactivityTimeout: the amount of time (in ms) after which, if no activity is noticed, a session will be considered stale
 // - staleSessionPurgeInterval: interval (in ms) at which stale sessions are purged i.e. found and forcibly logged out
+// - staleSessionForceLogout: whether or not we want to force log out and purge stale sessions
 //
 var staleSessionPurgeInterval = Meteor.settings && Meteor.settings.public && Meteor.settings.public.staleSessionPurgeInterval || (1*60*1000); // 1min
 var inactivityTimeout = Meteor.settings && Meteor.settings.public && Meteor.settings.public.staleSessionInactivityTimeout || (30*60*1000); // 30mins
+var forceLogout = Meteor.settings && Meteor.settings.public && Meteor.settings.public.staleSessionForceLogout || true;
 
 //
 // provide a user activity heartbeat method which stamps the user record with a timestamp of the last
@@ -26,10 +28,12 @@ Meteor.methods({
 //
 // periodically purge any stale sessions, removing their login tokens and clearing out the stale heartbeat.
 //
-Meteor.setInterval(function() {
-    var now = new Date(), overdueTimestamp = new Date(now-inactivityTimeout);
-    Meteor.users.update({heartbeat: {$lt: overdueTimestamp}},
-                        {$set: {'services.resume.loginTokens': []},
-                         $unset: {heartbeat:1}},
-                        {multi: true});
-}, staleSessionPurgeInterval);
+if (forceLogout) {
+    Meteor.setInterval(function() {
+        var now = new Date(), overdueTimestamp = new Date(now-inactivityTimeout);
+        Meteor.users.update({heartbeat: {$lt: overdueTimestamp}},
+                            {$set: {'services.resume.loginTokens': []},
+                             $unset: {heartbeat:1}},
+                            {multi: true});
+    }, staleSessionPurgeInterval);
+}
